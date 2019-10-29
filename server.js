00c84bc887a5dfd565992987851b094f19d6693e
Gabriel Torrandella <torrandellag@gmail.com>
2019-10-29 20:41:53 -0300

    Now server will bind with all IPs

--- server.js
+++ server.js
@@ -212,7 +212,7 @@ aplicacion.post('/checkLogin', [
 // Pone a escuchar al servidor y avisa por consola cuando esta listo
 port = 8080
 hostname = "localhost"
-server.listen(port, hostname, () => {
+server.listen(port, () => {
     console.log(`Stream server running at http://${hostname}:${port}/`);
 }); 
 
